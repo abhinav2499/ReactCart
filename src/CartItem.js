c7c2f7f23ffe9cb9ada8d09f46b65be03793a5c3
abhinav2499 <abhinavy2499@gmail.com>
2021-06-09 00:33:30 +0530

    added cart component

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,14 +11,30 @@ class CartItem extends React.Component {
         }
     }
     increaseQuantity = () => {
-        console.log(this.state);
+        // console.log(this.state);
+        this.setState((prevState)=>{
+            return {
+                qty: prevState.qty + 1
+            }
+        });
+    }
+    decreaseQuantity = () => {
+        const {qty} = this.state;
+        if(qty===0){
+            return ;
+        }
+        this.setState((prevState)=>{
+            return {
+                qty: prevState.qty - 1
+            }
+        });
     }
     render() {
         const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image}/>
+                    <img alt='' style={styles.image}/>
                 </div>
                 <div className="right-block">
                     <div style={{fontSize:25}}>{title}</div>
@@ -36,6 +52,7 @@ class CartItem extends React.Component {
                             className="action-icons" 
                             alt="decrease" 
                             src="https://image.flaticon.com/icons/png/128/992/992683.png" 
+                            onClick = {this.decreaseQuantity}
                         />
                         <img 
                             className="action-icons" 
